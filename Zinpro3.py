cbb330a15961ef18732eb6a31109e142a4cdfe2c
Marcos-zoo <62188418+Marcos-zoo@users.noreply.github.com>
2024-06-24 11:19:30 -0300

    Add files via upload

--- Zinpro3.py
+++ Zinpro3.py
@@ -10,8 +10,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from streamlit_option_menu import option_menu
-from numerize.numerize import numerize
-import gspread
+#from numerize.numerize import numerize
+#import gspread
 #from oauth2client.service_account import ServiceAccountCredentials
 
 # Set page configuration
